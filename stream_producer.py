87dff92140bf6ff1ae44b0c033b7143e0622bdee
ks-95 <42616789+ks-95@users.noreply.github.com>
2026-03-06 01:49:37 +0530

    Update stream_producer.py

--- stream_producer.py
+++ stream_producer.py
@@ -16,17 +16,14 @@ def generate_sale():
     }
 
 print("--- Starting Real-Time Sales Stream ---")
-
-# For now, let's print them to see it working
 try:
     while True:
         sale = generate_sale()
         print(f"New Sale: {sale}")
         
-        # In the next step, we will save this to a file for DuckDB to watch
         with open("live_stream.json", "a") as f:
             f.write(json.dumps(sale) + "\n")
             
-        time.sleep(1) # Simulates a 1-second delay between sales
+        time.sleep(1) 
 except KeyboardInterrupt:
     print("\nStream stopped.")
